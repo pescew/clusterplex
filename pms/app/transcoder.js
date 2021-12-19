c16694f56b57ddfd5026e194eaa3e1edc2726eb2
Walter Pesce <pescew@gmail.com>
2021-12-19 10:17:21 -0500

    extract and include mediainfo in job

--- pms/app/transcoder.js
+++ pms/app/transcoder.js
@@ -36,44 +36,48 @@ if (TRANSCODE_OPERATING_MODE == 'local') {
     if (TRANSCODER_VERBOSE == '1') {
         console.log('Setting VERBOSE to ON')
         setValueOf(newArgs, '-loglevel', 'verbose')
-        setValueOf(newArgs, '-loglevel_plex', 'verbose')    
+        setValueOf(newArgs, '-loglevel_plex', 'verbose')
     }
 
     let environmentVariables = process.env
     let workDir = process.cwd()
 
-    console.log(`Sending request to orchestrator on: ${ORCHESTRATOR_URL}`)
-    if (TRANSCODER_VERBOSE == '1') {
-        console.log(`cwd => ${JSON.stringify(workDir)}`)
-        console.log(`args => ${JSON.stringify(newArgs)}`)
-        console.log(`env => ${JSON.stringify(environmentVariables)}`)
-    }
-    
-    jobPoster.postJob(ORCHESTRATOR_URL, 
-    {
-        type: 'transcode',
-        payload: {
-            cwd: workDir,
-            args: newArgs,
-            env: environmentVariables
+    getMediaInfo(extractFilePath(process.argv.slice(2))).then(function (mediaInfo) {
+        console.log(`Sending request to orchestrator on: ${ORCHESTRATOR_URL}`)
+        if (TRANSCODER_VERBOSE == '1') {
+            console.log(`cwd => ${JSON.stringify(workDir)}`)
+            console.log(`args => ${JSON.stringify(newArgs)}`)
+            console.log(`env => ${JSON.stringify(environmentVariables)}`)
+            console.log(`mediaInfo => ${JSON.stringify(mediaInfo)}`)
         }
-    },
-    (response) => {
-        if (!response.result) {
-            console.error('Distributed transcoder failed, calling local')
-            if (TRANSCODE_OPERATING_MODE == 'both') {
-                transcodeLocally(process.cwd(), process.argv.slice(2), process.env);
-            } else {
-                // remote only
-                console.error(`Error transcoding and local transcode is disabled: TRANSCODE_OPERATING_MODE=${TRANSCODE_OPERATING_MODE}`)
-                process.exit(1)
+
+        jobPoster.postJob(ORCHESTRATOR_URL,
+            {
+                type: 'transcode',
+                payload: {
+                    cwd: workDir,
+                    args: newArgs,
+                    env: environmentVariables,
+                    mediaInfo: mediaInfo,
+                }
+            },
+            (response) => {
+                if (!response.result) {
+                    console.error('Distributed transcoder failed, calling local')
+                    if (TRANSCODE_OPERATING_MODE == 'both') {
+                        transcodeLocally(process.cwd(), process.argv.slice(2), process.env);
+                    } else {
+                        // remote only
+                        console.error(`Error transcoding and local transcode is disabled: TRANSCODE_OPERATING_MODE=${TRANSCODE_OPERATING_MODE}`)
+                        process.exit(1)
+                    }
+                } else {
+                    console.log("Remote Transcoding was successful")
+                    process.exit(0)
+                }
             }
-        } else {
-            console.log("Remote Transcoding was successful")
-            process.exit(0)
-        }
-    }
-    )
+        )
+    });
 }
 
 function transcodeLocally(cwd, args, env) {
@@ -92,6 +96,55 @@ function transcodeLocally(cwd, args, env) {
         console.log('Completed local transcode');
         process.exit(withErrors);
     });
+}
+
+function getMediaInfo(filePath) {
+    return new Promise(function (resolve, reject) {
+        let child = spawn(TRANSCODER_PATH + TRANSCODER_LOCAL_NAME, ['-hide_banner', '-i', filePath]);
+        let stderrData = []
+        child.stderr.on('data', (data) => {
+            stderrData = stderrData.concat(data);
+        });
+        child.stderr.on('end', () => {
+            let output = Buffer.concat(stderrData).toString();
+            let mediaInfo = {
+                'audioCodec': '',
+                'videoCodec': '',
+                'videoHeight': 0,
+                'videoWidth': 0,
+            }
+            let streams = ['Stream #0:0', 'Stream #0:1']
+            for (const stream of streams) {
+                let matchedStream = output.match(stream + '(.*), ')
+                if (matchedStream) {
+                    if (matchedStream[0].includes(" Video: ")) {
+                        let extractedPortion = matchedStream[0].match(' Video: [^,]+?(?: |,)')
+                        if (extractedPortion) {
+                            let extractedVideoCodec = extractedPortion[0].replace('Video: ', '').replace(',', '').trim()
+                            mediaInfo.videoCodec = extractedVideoCodec
+                        }
+                        let streamRes = matchedStream[0].match('(([\\d]{2,5}[x][\\d]{2,5}))')
+                        if (streamRes) {
+                            mediaInfo.videoWidth = parseInt(streamRes[0].split('x')[0], 10) || 0
+                            mediaInfo.videoHeight = parseInt(streamRes[0].split('x')[1], 10) || 0
+                        }
+                    } else if (matchedStream[0].includes(' Audio: ')) {
+                        let extractedPortion = matchedStream[0].match(' Audio: [^,]+?(?: |,)')
+                        if (extractedPortion) {
+                            let extractedAudioCodec = extractedPortion[0].replace('Audio: ', '').replace(',', '').trim()
+                            mediaInfo.audioCodec = extractedAudioCodec
+                        }
+                    }
+                }
+            }
+            resolve(mediaInfo)
+        });
+    });
+}
+
+function extractFilePath(argList) {
+    let indexFilePath = argList.findIndex(s => s == '-i') + 1
+    return (indexFilePath > 0 && argList) ? argList[indexFilePath] : ''
 }
 
 ON_DEATH( (signal, err) => {
